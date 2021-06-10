ad3f6bfc98d912776bc3a08e38b34ea46d8f0bbc
mdziaurkhan <mdziaurkhan@gmail.com>
2021-06-10 01:46:51 -0500

    adding 2nd project

---
+++ ansible-2_vlanCreation/ansible_2_vlanCreation_pb.py
@@ -0,0 +1,9 @@
+- hosts: switches
+  gather_facts: no
+  #ansible_network_os: yes
+
+  tasks:
+    - name: "Connectivity Check with Control machine by running ios commands"
+      ios_command:
+        commands:
+          - show run | i hostname
